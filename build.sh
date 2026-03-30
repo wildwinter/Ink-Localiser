23dc7202d99f57341c3a3bf34fd75d13b89deed9
wildwinter <ian@wildwinter.net>
2026-03-30 13:44:09 +0200

    Update with new vc version.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./publish/*
 
-version="0.0.4.1"
+version="0.0.4.2"
 targets=("osx-arm64" "osx-x64" "win-x86" "win-x64")
 
 for target in "${targets[@]}"; do
--- publish-nuget.sh
+++ publish-nuget.sh
@@ -12,7 +12,7 @@
 
 set -e
 
-VERSION="0.0.4.1"
+VERSION="0.0.4.2"
 if [ -z "$VERSION" ]; then
     echo "Error: version argument required."
     echo "Usage: publish-nuget.sh <version>"
